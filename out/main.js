42c904eed471bcea068020e7e63aae066464a686
0810diyor <diyorbekrozimboyev899@gmail.com>
2022-03-10 18:59:43 +0500

    wwww

--- out/main.js
+++ out/main.js
@@ -3,19 +3,35 @@ const balance = document.querySelector('#balance');
 const play = document.querySelector('#play');
 const bet = document.querySelector('#bet');
 const cards = document.querySelectorAll('.card');
+let cash = 100;
 let counter = 0;
+balance.innerText = `Balance $${cash}`;
 cards.forEach(card => {
     card.onclick = () => {
+        if (card.classList.contains('is-flipped')) {
+            return;
+        }
         if (counter < 3) {
             card.classList.add('is-flipped');
             counter++;
             let number = card.querySelector('.number');
             if (number.innerHTML === '$') {
+                let amount = +bet.value;
+                cash += amount * 1.5;
+                balance.innerText = `Balance $${cash}`;
                 setTimeout(() => {
                     alert('yutding!');
                 }, 1000);
                 counter = 4;
             }
+        }
+        else if (counter === 3) {
+            let amount = +bet.value;
+            cash = cash - amount;
+            balance.innerText = `Balance $${cash}`;
+            setTimeout(() => {
+                alert('yutqazdingiz!');
+            }, 1000);
         }
     };
 });
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,49 @@
 const balance: HTMLElement = document.querySelector('#balance')!
-const play: HTMLElement = document.querySelector('#play')!
-const bet: HTMLElement = document.querySelector('#bet')!
+const play: HTMLButtonElement = document.querySelector('#play')!
+const bet: HTMLInputElement = document.querySelector('#bet')!
 
 const cards: NodeListOf<HTMLElement> = document.querySelectorAll('.card')
 
+let cash:number = 100 
 let counter = 0
+
+
+
+balance.innerText = `Balance $${cash}`
+
 
 
 cards.forEach(card =>{
   card.onclick = () => {
+    if(card.classList.contains('is-flipped')){
+      return
+    }
+    
+
     if(counter < 3){
       card.classList.add('is-flipped')
       counter++
 
       let number = card.querySelector('.number')!
       if(number.innerHTML === '$'){
+        let amount = +bet.value
+        cash += amount * 1.5
+        balance.innerText = `Balance $${cash}`
+
         setTimeout(() =>{
           alert('yutding!')
         }, 1000)
         counter = 4
       }
+    }
+    else if(counter === 3){
+      let amount = +bet.value
+      cash = cash - amount
+      balance.innerText = `Balance $${cash}`
+      setTimeout(() =>{
+        alert('yutqazdingiz!')
+      }, 1000)
+      
     }
   }
 })
